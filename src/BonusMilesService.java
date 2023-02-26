6e9116b01e2ae5d0f4fca228a3777a8bedb50e06
Alex Baskakau <lenovos6000v4@gmail.com>
2023-02-26 15:36:26 +0000

    Working version

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+    public int calculate( int a ) {
+        int bonus;
+        bonus = a / 20;
+        return bonus;
+
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+       int price = 500;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
+       // System.out.println(service.calculate(1_000_000));
+    }
+}
