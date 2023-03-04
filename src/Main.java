58f1ac5fff7b6872e102152a26892be1985d98ea
Alex Baskakau <lenovos6000v4@gmail.com>
2023-03-04 14:54:55 +0000

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-       int price = 500;
+        int price = 500;
         int miles = service.calculate(price); // должно получиться 500
         System.out.println(miles);
-       // System.out.println(service.calculate(1_000));
+        // System.out.println(service.calculate(1_000));
     }
 }
